bfaa22388816975ec51717edbac0ae4a5ea4952c
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-07-26 12:20:39 -0300

    refactoring UI/UX

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -220,14 +220,14 @@ export default function Home() {
                     </span>
                     <span className="mx-4 w-[250px]">
                         <h1 className="text-2xl font-semibold">{user?.name}</h1>
-                        <b className="text-sm text-gray-400">FREE FOREVER</b>
+                        <b className="text-sm text-green-400 p-1">FREE FOREVER</b>
                     </span>
                 </div>
 
                 <div className="my-5">
                     <div className="uk-margin mb-2 flex items-center">
                         <input
-                            className="uk-input border rounded-sm text-white text-lg py-[25px] font-bold placeholder:font-bold"
+                            className="uk-input border rounded-sm text-white text-lg py-[25px] font-bold placeholder:font-medium"
                             type="text"
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
@@ -235,7 +235,7 @@ export default function Home() {
                             aria-label="search"
                         />
                         <div
-                            className='absolute right-7 w-[50px] text-xl rounded-sm p-2 py-[10px] bg-[#ffffff52] flex items-center justify-center'
+                            className='absolute right-7 w-[80px] text-xl rounded-sm p-2 py-[10px] bg-green-400 text-black flex items-center justify-center cursor-pointer'
                             onClick={() => searchSong()}
                         >
                             <IonIcon name="search" />
@@ -249,92 +249,94 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div className='flex mb-4'>
-                    <h2
-                        className="p-2 my-1 font-semibold border border-[#ffffff5b] mr-2 rounded-sm flex items-center"
-                        onClick={() => toogleLikedSongs()}
-                    >
-                        <IonIcon name="heart" />
-                        <p className='ml-1'>Minhas Músicas</p>
-                    </h2>
-                    <h2
-                        className='p-2 my-1 font-semibold border border-[#ffffff5b] mr-2 rounded-sm flex items-center'
-                        onClick={() => toogleSearchResults()}
-                    >
-                        <IonIcon name="search" />
-                        <p className='ml-1'>Resultados da Busca</p>
-                    </h2>
-                </div>
-                <div className={`overflow-y-scroll mb-2 ${isVisible ? 'h-[55vh]' : 'h-[100vh]'} ${isFetched ? 'uk-display-block h-[62vh]' : 'hidden h-[100vh]'}`}>
-
-                    {searchValues.map((result) => (
-
-                        <div
-                            key={result.videoId}
-                            onClick={() => getSong(result.videoId, result)}
-                            className="m-2 mx-2 p-2 cursor-pointer rounded-md flex items-center"
+                <div className='lg:flex justify-around'>
+                    <div className='flex mb-4 lg:block'>
+                        <h2
+                            className="p-2 my-1 font-semibold border-2 border-[#ffffff5b] mr-2 rounded-sm flex items-center cursor-pointer hover:border-green-500 lg:mb-2"
+                            onClick={() => toogleLikedSongs()}
                         >
-                            <span className="mr-4">
-                                <img
-                                    src={result.image}
-                                    className="max-w-14 h-14 object-cover rounded-sm uk-box-shadow-large"
-                                    alt=""
-                                />
-                            </span>
-                            <span>
-
-                                <h2 className="whitespace-nowrap text-ellipsis overflow-hidden w-[250px] font-bold">{result.title}</h2>
-                                <div className='flex items-center'>
-                                    <div className={`flex items-center ${result.title === currentSong?.info?.title ? 'block' : 'hidden'} text-green-400`}>
-                                        <div className='flex items-center text-[7pt]'>
-                                            <IonIcon name="ellipse" />
-                                        </div>
-                                        <p className='mr-2 ml-[2px] font-semibold whitespace-nowrap'>
-                                            Tocando Agora
-                                        </p>
-                                    </div>
-                                    <p className='whitespace-nowrap text-ellipsis overflow-hidden'>{result.author?.name}</p>
-                                </div>
-                            </span>
-                        </div>
-                    ))}
-                </div>
-
-                <div className={`overflow-y-scroll mb-2 ${isVisibleLikedSongs ? 'uk-display-block' : 'hidden h-[100vh]'} ${isVisible ? `h-[55vh]` : ''}`}>
-
-                    {user?.likedSongs?.map((song) => (
-
-                        <div
-                            key={song?.info?.videoId}
-                            onClick={() => getLikedSong(song)}
-                            className="m-2 mx-2 p-2 cursor-pointer rounded-md flex items-center"
+                            <IonIcon name="heart" />
+                            <p className='ml-1'>Minhas Músicas</p>
+                        </h2>
+                        <h2
+                            className='p-2 my-1 font-semibold border-2 border-[#ffffff5b] mr-2 rounded-sm flex items-center cursor-pointer hover:border-green-500'
+                            onClick={() => toogleSearchResults()}
                         >
-                            <span className="mr-4">
-                                <img
-                                    src={song?.info?.image}
-                                    className="max-w-14 h-14 object-cover rounded-sm uk-box-shadow-large"
-                                    alt=""
-                                />
-                            </span>
-                            <span>
-                                <h2 className="whitespace-nowrap text-ellipsis overflow-hidden w-[250px] font-bold">{song?.info?.title}</h2>
-                                <div className='flex items-center'>
-                                    <div className={`flex items-center ${song?.info?.title === currentSong?.info?.title ? 'block' : 'hidden'} text-green-400`}>
-                                        <div className='flex items-center text-[7pt]'>
-                                            <IonIcon name="ellipse" />
+                            <IonIcon name="search" />
+                            <p className='ml-1'>Resultados da Busca</p>
+                        </h2>
+                    </div>
+                    <div className={`overflow-y-scroll mb-2 ${isVisible ? 'h-[55vh]' : 'h-[100vh]'} ${isFetched ? 'uk-display-block h-[62vh]' : 'hidden h-[100vh]'} lg:w-[80%]`}>
+
+                        {searchValues.map((result) => (
+
+                            <div
+                                key={result.videoId}
+                                onClick={() => getSong(result.videoId, result)}
+                                className="m-2 mx-2 p-2 cursor-pointer rounded-md flex items-center"
+                            >
+                                <span className="mr-4">
+                                    <img
+                                        src={result.image}
+                                        className="max-w-12 h-12 object-cover rounded-sm uk-box-shadow-large"
+                                        alt=""
+                                    />
+                                </span>
+                                <span>
+
+                                    <h2 className="whitespace-nowrap text-ellipsis overflow-hidden w-[250px] font-semibold text-[11pt]">{result.title}</h2>
+                                    <div className='flex items-center'>
+                                        <div className={`flex items-center ${result.title === currentSong?.info?.title ? 'block' : 'hidden'} text-green-400`}>
+                                            <div className='flex items-center text-[7pt]'>
+                                                <IonIcon name="ellipse" />
+                                            </div>
+                                            <p className='mr-2 ml-[2px] font-semibold whitespace-nowrap text-[10pt]'>
+                                                Tocando Agora
+                                            </p>
                                         </div>
-                                        <p className='mr-2 ml-[2px] font-semibold whitespace-nowrap'>
-                                            Tocando Agora
-                                        </p>
+                                        <p className='whitespace-nowrap text-ellipsis overflow-hidden text-[10pt] font-light'>{result.author?.name}</p>
                                     </div>
-                                    <p className='whitespace-nowrap text-ellipsis overflow-hidden'>{song?.info?.author?.name}</p>
-                                </div>
-                            </span>
-                        </div>
-                    ))}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className={`overflow-y-scroll mb-2 ${isVisibleLikedSongs ? 'uk-display-block' : 'hidden h-[100vh]'} ${isVisible ? `h-[55vh]` : ''} lg:w-[80%]`}>
+
+                        {user?.likedSongs?.map((song) => (
+
+                            <div
+                                key={song?.info?.videoId}
+                                onClick={() => getLikedSong(song)}
+                                className="m-2 mx-2 p-2 cursor-pointer rounded-md flex items-center"
+                            >
+                                <span className="mr-4">
+                                    <img
+                                        src={song?.info?.image}
+                                        className="max-w-12 h-12 object-cover rounded-sm uk-box-shadow-large"
+                                        alt=""
+                                    />
+                                </span>
+                                <span>
+                                    <h2 className="whitespace-nowrap text-ellipsis overflow-hidden w-[250px] font-semibold">{song?.info?.title}</h2>
+                                    <div className='flex items-center'>
+                                        <div className={`flex items-center ${song?.info?.title === currentSong?.info?.title ? 'block' : 'hidden'} text-green-400`}>
+                                            <div className='flex items-center text-[7pt]'>
+                                                <IonIcon name="ellipse" />
+                                            </div>
+                                            <p className='mr-2 ml-[2px] font-semibold whitespace-nowrap text-[10pt]'>
+                                                Tocando Agora
+                                            </p>
+                                        </div>
+                                        <p className='whitespace-nowrap text-ellipsis overflow-hidden text-[11pt]'>{song?.info?.author?.name}</p>
+                                    </div>
+                                </span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
 
-                <div className={`w-[90%] uk-position-fixed bottom-4 rounded-sm bg-[#f1f3f4] text-slate-900 uk-box-shadow-xlarge ${isVisible ? 'uk-display-block' : 'hidden'}`}>
+                <div className={`w-auto uk-position-fixed bottom-4 rounded-sm bg-[#f1f3f4] text-slate-900 uk-box-shadow-xlarge ${isVisible ? 'block' : 'hidden'} lg:w-[95%] lg:flex lg:items-center justify-center`}>
                     <div className="flex justify-around m-2 pb-2">
                         <span>
                             <img className="max-w-14 h-14 object-cover rounded-sm uk-box-shadow-xlarge" src={currentSong?.info?.image} alt="" />
